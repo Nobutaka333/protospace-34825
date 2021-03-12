467cc8914db18137ade1a95d88b42d054652d013
Nobutaka333 <taka9861+techcamp@gmail.com>
2021-03-12 23:46:09 +0900

    プロトタイプ一覧修正。newprotoから保存ができなかった修正。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @name = user.name
-    @prototypes = user.prototypes
-    @profile = user.profile
-    @occupation = user.occupation
-    @position = user.position
+    @user = User.find(params[:id])
+    @prototypes = @user.prototypes
   end
 end
